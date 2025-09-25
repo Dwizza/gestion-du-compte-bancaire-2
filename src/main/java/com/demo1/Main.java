47d7451015f5bf0b56220a37f502f109cb9b09af
Oussama Errahili <oussamaerrahili124@gmail.com>
2025-09-25 16:23:52 +0100

    Add initial project configuration files and main application structure

---
+++ src/main/java/com/demo1/Main.java
@@ -0,0 +1,44 @@
+package com.demo1;
+
+import com.demo1.Controllers.AuthController;
+
+import java.sql.Connection;
+
+import java.sql.SQLException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("=== Bienvenue dans la Banque Digitale ===");
+        showLoginMenu();
+    }
+
+    public static void showLoginMenu(){
+        System.out.println("=====Menu Principale======");
+        System.out.println("1. Login");
+        System.out.println("2. Exit");
+        System.out.print("Your choice: ");
+
+        String choice = scanner.nextLine();
+
+        switch(choice){
+            case"1":
+                AuthController.Login();
+                break;
+            case "2":
+                System.out.println("Good bye!");
+                System.exit(0);
+                break;
+        }
+    }
+
+
+}
+
+
